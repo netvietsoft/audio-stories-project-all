2a35fe48355cff40ff7582c9b4e02dfbdbd83351
tienchung1704 <tien.chungloveu@gmail.com>
2026-03-05 15:27:59 +0700

    add user, stories list endpoint

---
+++ be/ecosystem.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    apps: [
+        {
+            name: "auth-be",
+            script: "./dist/main.js",
+            instances: 1,
+            exec_mode: "cluster",
+            autorestart: true,
+            watch: false,
+            max_memory_restart: "1G",
+            env: {
+                PORT: 3000,
+                NODE_ENV: "production",
+            },
+            error_file: "./logs/err.log",
+            out_file: "./logs/out.log",
+            log_file: "./logs/combined.log",
+            time: true,
+        },
+    ],
+};

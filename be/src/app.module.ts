086fe6d9d0aa497502630311db5647b5d5b54afe
tienchung1704 <tien.chungloveu@gmail.com>
2026-03-09 11:06:21 +0700

    fix conflick

--- be/src/app.module.ts
+++ be/src/app.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+﻿import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { ScheduleModule } from '@nestjs/schedule';
 import { AppController } from './app.controller';
@@ -11,15 +11,12 @@ import { StoriesModule } from './stories/stories.module';
 import { CategoriesModule } from './categories/categories.module';
 import { AuthorsModule } from './authors/authors.module';
 import { ChaptersModule } from './chapters/chapters.module';
-<<<<<<< HEAD
 import { TransactionsModule } from './transactions/transactions.module';
 import { CommentsModule } from './comments/comments.module';
 import { SettingsModule } from './settings/settings.module';
 import { MembershipsModule } from './memberships/memberships.module';
 import { PackagesModule } from './packages/packages.module';
-=======
 import { UserFeaturesModule } from './user-features/user-features.module';
->>>>>>> master
 
 @Module({
   imports: [
@@ -36,20 +33,14 @@ import { UserFeaturesModule } from './user-features/user-features.module';
     CategoriesModule,
     AuthorsModule,
     ChaptersModule,
-<<<<<<< HEAD
     TransactionsModule,
     CommentsModule,
     SettingsModule,
     MembershipsModule,
     PackagesModule,
-=======
     UserFeaturesModule,
->>>>>>> master
   ],
-
-
-
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule { }
+export class AppModule {}

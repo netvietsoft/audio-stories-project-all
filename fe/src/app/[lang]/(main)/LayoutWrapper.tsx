6c561cf9877adc86dd8b9a0b25eebd572e66a97d
hoanglam <boluclac1301@gmail.com>
2026-03-21 17:31:09 +0700

    Deploy FE: 2026-03-21 17:31:08

--- fe/src/app/[lang]/(main)/LayoutWrapper.tsx
+++ fe/src/app/[lang]/(main)/LayoutWrapper.tsx
@@ -5,10 +5,10 @@ import React from "react";
 export default function LayoutWrapper({ children }: { children: React.ReactNode }) {
 
     return (
-        <main
-            className="flex-1 w-full bg-slate-50 pb-24 dark:bg-slate-950 mx-auto px-4 sm:px-6 xl:max-w-[1400px] 2xl:w-[70vw] 2xl:max-w-[70vw] py-8"
-        >
-            {children}
+        <main className="flex-1 w-full bg-white dark:bg-black">
+            <div className="mx-auto w-full bg-white pb-24 px-4 sm:px-6 xl:max-w-[1400px] 2xl:w-[70vw] 2xl:max-w-[70vw] py-8 dark:bg-black">
+                {children}
+            </div>
         </main>
     );
 }

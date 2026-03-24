90ed3799963dcf52a129ed0872cfb2159ede8e3f
tienchung1704 <tien.chungloveu@gmail.com>
2026-03-24 10:39:15 +0700

    delete temp file

--- fe/fix.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-const file = 'c:\\auth-be\\fe\\src\\app\\[lang]\\admin\\stories\\[id]\\chapters\\_components\\ChapterForm.tsx';
-let content = fs.readFileSync(file, 'utf8');
-content = content.replace('const ReactQuill = dynamic(', 'const ReactQuill: any = dynamic(');
-fs.writeFileSync(file, content);
-console.log('Fixed ChapterForm.tsx');
